ecbc8a4d91ccc8e0849117dce414df156791aa5a
chuncatz <95894169+chuncatz@users.noreply.github.com>
2021-12-24 14:09:33 +0800

    First Commit

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.8563248257580973,
+        "pitch": -0.33388612990864885,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9898838280662687,
+          "pitch": -0.06231493072376715,
+          "rotation": 5.497787143782138,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6153568005708969,
+          "pitch": -0.43442953298956866,
+          "title": "Lee Kong Chian Natural History Museum",
+          "text": "The only museum in Singapore with real dinosaur fossils!"
+        },
+        {
+          "yaw": 1.9118282507161224,
+          "pitch": -0.27680086638507717,
+          "title": "NUS Museum",
+          "text": "Not to be confused with the museum with dinosaurs"
+        },
+        {
+          "yaw": 2.2599100292720733,
+          "pitch": 0.11522007813341162,
+          "title": "Alice Lee Plaza",
+          "text": "Located right outside YST conservatory. This is a place where students hang out or take their favourite grad shoots after their commencement ceremony at UCC."
+        },
+        {
+          "yaw": -2.944350605140535,
+          "pitch": -0.36140707830066177,
+          "title": "Welcome to Yong Siew Toh Conservatory of Music!",
+          "text": "<div>Officially opened in 2006,</div><div>the YST Conservatory building houses state-of-the-art facilities for training professional musicians.</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.3079728675190765,
+        "pitch": -0.03927000004087411,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.06463066792554,
+          "pitch": 0.02328187685263927,
+          "rotation": 0.7853981633974483,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.963391938072558,
+          "pitch": -0.11498315536103831,
+          "title": "Come explore our scenic Conservatory Foyer!",
+          "text": "Located directly outside the Concert Hall and near the building entrance, the Foyer is an elegant space with high ceilings and a pleasing view. The Foyer space is suitable for both informal (standing) and formal (sit-down) receptions."
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8779699579084337,
+        "pitch": 0.0015144238919475583,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.4238038041513894,
+          "pitch": 0.25962484124714535,
+          "title": "Conservatory Lounge",
+          "text": "The Conservatory Lounge is a comfortable space that invites visitors to sit down, relax and take in their surroundings. The Lounge is suitable for VIP cocktail receptions of not more than 100 persons."
+        },
+        {
+          "yaw": -3.1197307320962473,
+          "pitch": -0.09958539179753068,
+          "title": "Need a quick bite?",
+          "text": "A Tempo café sells a selection of food and beverages in the daytime on weekdays, and is open after hours during selected events."
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
